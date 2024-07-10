1a9e809ebd447e7bf22fbbbab4590821f084b792
TCzerny <tczerny@mac.com>
2024-07-10 18:28:23 +0200

    Enable Switch for Host 2 & 3
    now you can enable/disable Host 2 & 3

--- modbus-proxy/CHANGELOG.md
+++ modbus-proxy/CHANGELOG.md
@@ -2,11 +2,4 @@
 
 | Version | Change                                                      |
 | ------- | ----------------------------------------------------------- |
-| 1.0.18  | switching to proper workdir before installing venv          |
-| 1.0.17  | use of venv optional. it will use it only on rebuild        |
-| 1.0.16  | switched to using a venv for running modbus proxy           |
-| 1.0.15  | connection time variable wrongly named                      |
-| 1.0.14  | introduced options for timeout, connectiontime and logging  |
-| 1.0.13  | fixed issue s6-overlay-suexec: fatal: can only run as pid 1 |
-| 1.0.12  |  Indentation error in config.yaml fixed                     |
-| 1.0.11  |  Release Version. Packaged modbusproxy + README and Logo    |
+| 1.0.0   | Initial Fork Version with three modbus host configurations  |
--- modbus-proxy/run.sh
+++ modbus-proxy/run.sh
@@ -11,6 +11,7 @@ CONFIG_CONNECTIONTIME=$(bashio::config 'connection_time')
 CONFIG_LOGLEVEL=$(bashio::config 'loglevel')
 
 # Create second Config
+CONFIG_ENABLEHOST2=$(bashio::config 'enablehost2') 
 CONFIG_HOST2=$(bashio::config 'upstreamhost2')
 CONFIG_PORT2=$(bashio::config 'upstreamport2')
 CONFIG_LISTENPORT2=$(bashio::config 'listenport2')
@@ -18,53 +19,84 @@ CONFIG_TIMEOUT2=$(bashio::config 'timeout2')
 CONFIG_CONNECTIONTIME2=$(bashio::config 'connection_time2')
 
 # Create third Config
+CONFIG_ENABLEHOST3=$(bashio::config 'enablehost3') 
 CONFIG_HOST3=$(bashio::config 'upstreamhost3')
 CONFIG_PORT3=$(bashio::config 'upstreamport3')
 CONFIG_LISTENPORT3=$(bashio::config 'listenport3')
 CONFIG_TIMEOUT3=$(bashio::config 'timeout3')
 CONFIG_CONNECTIONTIME3=$(bashio::config 'connection_time3')
 
-echo "Preparing to run modbus-proxy"
+echo "Preparing configuration to run modbus-proxy"
+echo "Loglevel: $CONFIG_LOGLEVEL"
+echo " "
+echo "Preparing Upstream Host 1"
 echo "Upstream: $CONFIG_HOST:$CONFIG_PORT"
 echo "Listen: $CONFIG_LISTENPORT"
 echo "Timeout: $CONFIG_TIMEOUT"
 echo "Connection Time: $CONFIG_CONNECTIONTIME"
-echo "Loglevel: $CONFIG_LOGLEVEL"
 
-echo "Upstream 2: $CONFIG_HOST2:$CONFIG_PORT2"
-echo "Listen 2: $CONFIG_LISTENPORT2"
-echo "Timeout 2: $CONFIG_TIMEOUT2"
-echo "Connection Time 2: $CONFIG_CONNECTIONTIME2"
-
-echo "Upstream 3: $CONFIG_HOST3:$CONFIG_PORT3"
-echo "Listen 3: $CONFIG_LISTENPORT3"
-echo "Timeout 3: $CONFIG_TIMEOUT3"
-echo "Connection Time 3: $CONFIG_CONNECTIONTIME3"
+echo "devices:" > ./modbus.txt
+echo "  - modbus:" >> ./modbus.txt
+echo "      url: ${CONFIG_HOST}:${CONFIG_PORT} # device url (mandatory)" >> modbus.txt
+echo "      timeout: ${CONFIG_TIMEOUT} # communication timeout (s) (optional, default: 10)" >> modbus.txt
+echo "      connection_time: ${CONFIG_CONNECTIONTIME} # delay after connection (s) (optional, default: 0)" >> modbus.txt
+echo "    listen:" >> modbus.txt
+echo "      bind: 0:${CONFIG_LISTENPORT} # listening address (mandatory)" >> modbus.txt
 
 
-sed -i "s/__HOST__/${CONFIG_HOST}/g" ./modbus.config.yaml
-sed -i "s/__PORT__/${CONFIG_PORT}/g" ./modbus.config.yaml
-sed -i "s/__LISTENPORT__/${CONFIG_LISTENPORT}/g" ./modbus.config.yaml
-sed -i "s/__TIMEOUT__/${CONFIG_TIMEOUT}/g" ./modbus.config.yaml
-sed -i "s/__CONNECTIONTIME__/${CONFIG_CONNECTIONTIME}/g" ./modbus.config.yaml
-sed -i "s/__LOGLEVEL__/${CONFIG_LOGLEVEL}/g" ./modbus.config.yaml
+if [ $CONFIG_ENABLEHOST2 = true ]
+then
+    echo " "
+    echo "Preparing Upstream Host 2"
+    echo "Upstream 2: $CONFIG_HOST2:$CONFIG_PORT2"
+    echo "Listen 2: $CONFIG_LISTENPORT2"
+    echo "Timeout 2: $CONFIG_TIMEOUT2"
+    echo "Connection Time 2: $CONFIG_CONNECTIONTIME2"
+
+    echo "  - modbus:" >> ./modbus.txt
+    echo "      url: ${CONFIG_HOST2}:${CONFIG_PORT2} # device url (mandatory)" >> modbus.txt
+    echo "      timeout: ${CONFIG_TIMEOUT2} # communication timeout (s) (optional, default: 10)" >> modbus.txt
+    echo "      connection_time: ${CONFIG_CONNECTIONTIME2} # delay after connection (s) (optional, default: 0)" >> modbus.txt
+    echo "    listen:" >> modbus.txt
+    echo "      bind: 0:${CONFIG_LISTENPORT2} # listening address (mandatory)" >> modbus.txt
+
+fi
+
+if [ $CONFIG_ENABLEHOST3 = true ] 
+then
+    echo " "
+    echo "Preparing Upstream Host 3"
+    echo "Upstream 3: $CONFIG_HOST3:$CONFIG_PORT3"
+    echo "Listen 3: $CONFIG_LISTENPORT3"
+    echo "Timeout 3: $CONFIG_TIMEOUT3"
+    echo "Connection Time 3: $CONFIG_CONNECTIONTIME3"
+
+    echo "  - modbus:" >> ./modbus.txt
+    echo "      url: ${CONFIG_HOST3}:${CONFIG_PORT3} # device url (mandatory)" >> modbus.txt
+    echo "      timeout: ${CONFIG_TIMEOUT3} # communication timeout (s) (optional, default: 10)" >> modbus.txt
+    echo "      connection_time: ${CONFIG_CONNECTIONTIME3} # delay after connection (s) (optional, default: 0)" >> modbus.txt
+    echo "    listen:" >> modbus.txt
+    echo "      bind: 0:${CONFIG_LISTENPORT3} # listening address (mandatory)" >> modbus.txt
+
+fi
 
 
-sed -i "s/__HOST2__/${CONFIG_HOST2}/g" ./modbus.config.yaml
-sed -i "s/__PORT2__/${CONFIG_PORT2}/g" ./modbus.config.yaml
-sed -i "s/__LISTENPORT2__/${CONFIG_LISTENPORT2}/g" ./modbus.config.yaml
-sed -i "s/__TIMEOUT2__/${CONFIG_TIMEOUT2}/g" ./modbus.config.yaml
-sed -i "s/__CONNECTIONTIME2__/${CONFIG_CONNECTIONTIME2}/g" ./modbus.config.yaml
+echo "logging:" >> modbus.txt
+echo "  version: 1" >> modbus.txt
+echo "  formatters: " >> modbus.txt
+echo "    standard:" >> modbus.txt
+echo '      format: "%(asctime)s %(levelname)8s %(name)s: %(message)s"' >> modbus.txt
+echo "  handlers:" >> modbus.txt
+echo "    console:" >> modbus.txt
+echo "      class: logging.StreamHandler" >> modbus.txt
+echo "      formatter: standard" >> modbus.txt
+echo "  root:" >> modbus.txt
+echo "    handlers: ['console']" >> modbus.txt
+echo "    level: ${CONFIG_LOGLEVEL}" >> modbus.txt
 
 
-sed -i "s/__HOST3__/${CONFIG_HOST3}/g" ./modbus.config.yaml
-sed -i "s/__PORT3__/${CONFIG_PORT3}/g" ./modbus.config.yaml
-sed -i "s/__LISTENPORT3__/${CONFIG_LISTENPORT3}/g" ./modbus.config.yaml
-sed -i "s/__TIMEOUT3__/${CONFIG_TIMEOUT3}/g" ./modbus.config.yaml
-sed -i "s/__CONNECTIONTIME3__/${CONFIG_CONNECTIONTIME3}/g" ./modbus.config.yaml
-
-
-echo "Generated Config"
+echo "Generated Config:"
+cat modbus.txt > ./modbus.config.yaml
 cat ./modbus.config.yaml
 
 if [ -f "./venv/bin/activate" ] ; then
